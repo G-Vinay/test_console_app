90e242a771ce25fc6f3b8b75eb0dba9fe0ab62f2
G-Vinay <vinay.gundavaram@gmail.com>
2024-07-18 18:27:06 +0530

    Added test user credentials in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+string test_username = "vinay123";
+string test_user_password = "Vinay@123";
 
 Console.WriteLine("Hello..");
+
+Console.WriteLine("Test Username:" + test_username);
+Console.WriteLine("Test User Password:" + test_user_password);
