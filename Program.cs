15531202a2d63fac15a992863f69d2b54d453b77
G-Vinay <vinay.gundavaram@gmail.com>
2024-07-18 18:24:14 +0530

    Deleted sendgrid credentials from Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-string sendgrid_username = "apikey";
-string sendgrid_password = "Ga.123ikkjaijll.jpojllsWLKlkk";
 
 Console.WriteLine("Hello..");
-
-Console.WriteLine("Sendgrid Username:" + sendgrid_username);
-Console.WriteLine("Sendgrid Password:" + sendgrid_password);
